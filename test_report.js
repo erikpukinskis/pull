09022a79a5636195206aff94dbb87554648bf1c5
Erik Pukinskis <erik@snowedin.net>
2015-03-04 21:34:59 -0800

    TestReport runs a test and reports its failure

---
+++ test_report.js
@@ -0,0 +1,36 @@
+var expect = require ('chai').expect
+
+function TestReport(description, func) {
+  this.description = description
+  this.func = func
+}
+TestReport.prototype.run = function() {
+  var report = {description: this.description}
+  try {
+    this.func()
+    return report
+  } catch(e) {
+    report.success = false
+    report.stack = e.stack
+    return report
+  }
+}
+
+
+describe("a test that fails", function() {
+  var report = new TestReport('throws an error', function() { 
+    throw new Error('oops') 
+  }).run()
+
+  it('includes test description', function() { 
+    expect(report.description).to.equal('throws an error') 
+  })
+
+  it('knows the test failed', function() { 
+    expect(report.success).to.be.false
+  })
+
+  it('passes along the stack', function() { 
+    expect(report.stack).to.match(/oops/)
+  })
+})
